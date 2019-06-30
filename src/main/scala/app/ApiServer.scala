6f4df5247935424664334be70dcaa5c3f68bb9ce
Yasuhiro Suzuki <yasszu@yahoo.com>
2019-07-01 02:03:43 +0900

    refactor ApiServer

--- src/main/scala/app/ApiServer.scala
+++ src/main/scala/app/ApiServer.scala
@@ -3,7 +3,7 @@ package app
 import app.controller.UserController
 import app.mysql.DDL
 import app.mysql.Impl._
-import com.twitter.finagle.Http
+import com.twitter.finagle.{Http, ListeningServer}
 import com.twitter.finagle.mysql._
 import com.twitter.server.TwitterServer
 import com.twitter.util.{Await, Future}
@@ -14,19 +14,20 @@ object ApiServer extends TwitterServer {
 
   override def failfastOnFlagsNotParsed: Boolean = true
 
-  lazy val userController = UserController()
+  lazy val userController: UserController = UserController()
+
+  lazy val apiServer: ListeningServer = Http.server.serve(":8080", userController.endpoints.toService)
 
   def createTables()(implicit client: Client): Future[Result] = {
     client.query(DDL.createUsersTable)
   }
 
   def main() {
-    val server = Http.server.serve(":8080", userController.endpoints.toService)
     onExit{
-      server.close()
+      apiServer.close()
     }
     Await.result(createTables())
-    Await.ready(server)
+    Await.ready(apiServer)
   }
 
 }
